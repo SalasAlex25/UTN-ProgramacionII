688d2b49c428ec4c36358e75d16b9d280d3e04fa
SalasAlex25 <salasalexaldair@gmail.com>
2022-09-27 22:28:40 -0300

    Primer Commit

---
+++ Alta_recetas/RecetasSLN/dominio/Ingrediente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecetasSLN.dominio
+{
+    class Ingrediente
+    {
+        public int IngredienteId { get; set; }
+        public string Nombre { get; set; }
+
+        public string Unidad { get; set; }
+
+        public Ingrediente(int ingredienteID, string nombre ,string unidad)
+        {
+            this.IngredienteId = ingredienteID;
+            this.Nombre = nombre;
+            this.Unidad = unidad;
+        }
+
+
+        public override string ToString()
+        {
+            return Nombre;
+        }
+
+    }
+}
---
+++ Alta_recetas/RecetasSLN/dominio/Receta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecetasSLN.dominio
+{
+     class Receta
+    {
+        public int recetaNro { get; set; }
+        public string nombre { get; set; }
+        public int tipoReceta { get; set; }
+        public string cheff { get; set; }
+        public List<DetalleReceta> DetalleRecetas { get; set; }
+
+        public Receta()
+        {
+            DetalleRecetas = new List<DetalleReceta>();
+        }
+
+        public void AgregarReceta(DetalleReceta detalle)
+        {
+            DetalleRecetas.Add(detalle);
+        }
+
+        public void Eliminar(int indice)
+        {
+            DetalleRecetas.RemoveAt(indice);
+        }
+
+
+    }
+}
